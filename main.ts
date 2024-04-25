34a811bd35d62dae14899419ed204647001cf1f4
Imran11055 <108211026+Imran11055@users.noreply.github.com>
2024-04-25 20:15:17 +0500

    Push My Student Managment Code

---
+++ main.ts
@@ -0,0 +1,117 @@
+#! /usr/bin/env node
+class studentData {
+    name: string;
+    StudentID: string;
+    courses: string[];
+    balance: number;
+
+    constructor(name: string) {
+        this.name = name;
+        this.StudentID = this.genID();
+        this.courses = [];
+        this.balance = 0;
+    }
+    private genID(): string {
+       return "S" + Math.floor(10000 + Math.random() * 90000)
+    }
+    enroll(course: string): void {
+        this.courses.push(course)
+    }
+    checkBalance():number {
+        return this.balance
+    }
+    tuitionFee(amount: number): void {
+        if (amount > this.balance) {
+            console.log("Insufficient balance.\n Please add funds.");
+        } else {
+            this.balance -= amount;
+            console.log("Payment successful.\n Your Remaining balance:", this.balance);
+        }
+    }
+    status(): void {
+      console.log("Student Name: ", this.name)
+      console.log("Student ID: ", this.StudentID)
+      console.log("Course Enrolled: ", this.courses.join(", "))
+      console.log("Current Balance: ", this.balance)
+      
+    }
+}
+
+class subj {
+    code: string;
+    name: string;
+    fee: number;
+
+    constructor(code:string, name: string, fee: number) {
+        this.code = code;
+        this.name = name;
+        this.fee = fee
+    }
+}
+
+const c1 = new subj("py", "Python", 50000)
+const c2 = new subj("ts", "Typescript", 45000)
+const c3 = new subj("wd", "Web Develpment", 300000)
+const c4 = new subj("cs", "Cyber Security", 150000)
+const c5 = new subj("ml", "Machine Learning", 200000)
+const c6 = new subj("web", "Web 3.0", 120000)
+ 
+const Student1 = new studentData("Ali")
+const Student2 = new studentData("Hassan")
+const Student3 = new studentData("Hussain")
+const Student4 = new studentData("Abbas")
+const Student5 = new studentData("Hadi")
+
+Student1.enroll(c1.name)
+Student1.enroll(c2.name)
+
+Student2.enroll(c1.name)
+Student2.enroll(c5.name)
+Student2.enroll(c6.name)
+
+Student3.enroll(c4.name)
+
+Student4.enroll(c6.name)
+
+Student5.enroll(c1.name)
+Student5.enroll(c2.name)
+Student5.enroll(c3.name)
+Student5.enroll(c6.name)
+
+Student1.balance = 5700
+Student1.tuitionFee(c1.fee)
+Student1.tuitionFee(c2.fee)
+
+Student2.balance = 50000
+Student2.tuitionFee(c1.fee)
+Student2.tuitionFee(c5.fee)
+Student2.tuitionFee(c6.fee)
+
+Student3.balance = 7000
+Student3.tuitionFee(c4.fee)
+
+Student4.balance = 60000
+Student4.tuitionFee(c6.fee)
+
+Student5.balance = 70000
+Student5.tuitionFee(c1.fee)
+Student5.tuitionFee(c2.fee)
+Student5.tuitionFee(c3.fee)
+Student5.tuitionFee(c6.fee)
+
+
+
+console.log("\nStudent 1 Status:");
+Student1.status()
+
+console.log("\nStudent 2 Status:");
+Student2.status()
+
+console.log("\nStudent 3 Status:");
+Student3.status()
+
+console.log("\nStudent 4 Status:");
+Student4.status()
+
+console.log("\nStudent 5 Status:");
+Student5.status()
